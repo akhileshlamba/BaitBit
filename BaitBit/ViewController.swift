3c1fe08a0c69a8c6acffb0c86b3edb07dae7167a
Akhilesh <c1akhileshlamba02@gmail.com>
2019-03-30 14:03:04 +1100

    Initial Commit

---
+++ BaitBit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaitBit
+//
+//  Created by Akhilesh Lamba on 30/3/19.
+//  Copyright © 2019 The Hawks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
