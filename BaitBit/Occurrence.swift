0c6859a0405e4cc9185bd0047a6725e1feb62c09
Xiaotian LIU <sheldon.liuxiaotian@gmail.com>
2019-04-04 10:18:55 +1100

    create a new Class Occurrence to represent occurrence record of invasive species in the app

---
+++ BaitBit/Occurrence.swift
@@ -0,0 +1,22 @@
+//
+//  Occurrence.swift
+//  BaitBit
+//
+//  Created by Xiaotian LIU on 4/4/19.
+//  Copyright © 2019 The Hawks. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Occurrence: NSObject {
+    var location: CLLocationCoordinate2D?
+    var eventDate: NSDate?
+    var species: String?
+    
+    init(location: CLLocationCoordinate2D, eventDate: NSDate, species: String) {
+        self.location = location
+        self.eventDate = eventDate
+        self.species = species
+    }
+}
