d29d724bf079a6f3d3d549466704a82719b18cc4
Xiaotian LIU <sheldon.liuxiaotian@gmail.com>
2019-04-23 20:13:34 +1000

    write methods for Program entity class

--- BaitBit/Bait.swift
+++ BaitBit/Bait.swift
@@ -13,6 +13,25 @@ enum BaitStatus: Int, CaseIterable {
 }
 
 class Bait: NSObject {
+    
+    var id: String
+    var laidDate: NSDate?
+    var latitude: Double
+    var longitude: Double
+    var photoPath: String?
+    var program: Program?
+    var status: BaitStatus
+    
+    init(id: String, laidDate: NSDate, latitude: Double, longitude: Double, photoPath: String, program: Program, status: BaitStatus) {
+        self.id = id
+        self.laidDate = laidDate
+        self.latitude = latitude
+        self.longitude = longitude
+        self.photoPath = photoPath
+        self.program = program
+        self.status = status
+    }
+    
     var isOverdue: Bool {
         return false
     }
--- BaitBit/Program.swift
+++ BaitBit/Program.swift
@@ -9,43 +9,65 @@
 import UIKit
 
 class Program: NSObject {
-    var programId: Int
+    var id: String
     var baitType: String?
     var species: String?
     var startDate: NSDate?
-    var active: Bool
-    var baits: [Bait]?
+    var isActive: Bool
+    var baits: [String : Bait] = [:]
     
-    init(programId: Int, baitType: String, species: String, startDate: NSDate) {
-        self.programId = programId
+    init(id: String, baitType: String, species: String, startDate: NSDate, isActive: Bool) {
+        self.id = id
         self.baitType = baitType
         self.species = species
         self.startDate = startDate
-        self.active = true
+        self.isActive = isActive
+    }
+    
+    func addToBaits(bait: Bait) {
+        if self.baits.keys.contains(bait.id) {
+            return
+        } else {
+            self.baits[bait.id] = bait
+        }
+    }
+    
+    func removeFromBaits(bait: Bait) -> Bait? {
+        return self.baits.removeValue(forKey: bait.id)
     }
     
     var numberOfActiveBaits: Int {
-        // TODO: implement number of active baits for a program
-        return 0
+        return self.baits.filter { (element) -> Bool in
+            return element.value.isActive
+        }.count
     }
     
     var numberOfOverdueBaits: Int {
-        // TODO: implement number of overdue baits for a program
-        return 0
+        return self.baits.filter({ (element) -> Bool in
+            return element.value.isOverdue
+        }).count
     }
     
     var numberOfDueSoonBaits: Int {
-        // TODO: implement number of due soon baits for a program
-        return 0
+        return self.baits.filter({ (element) -> Bool in
+            return element.value.isDueSoon
+        }).count
     }
     
     var numberOfAllBaits: Int {
-        return self.baits!.count
+        return self.baits.count
     }
     
     var numberOfUnremovedBaits: Int {
-        // TODO: implement number of unremoved baits for a program
-        return 0
+        return self.baits.filter({ (element) -> Bool in
+            return !element.value.isRemoved
+        }).count
+    }
+    
+    var numberOfRemovedBaits: Int {
+        return self.baits.filter({ (element) -> Bool in
+            return element.value.isRemoved
+        }).count
     }
     
     var hasOverdueBaits: Bool {
