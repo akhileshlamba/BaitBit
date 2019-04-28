b32bed02dd6723a3bdffa79fe768fa3d8c1ad2e4
Xiaotian LIU <sheldon.liuxiaotian@gmail.com>
2019-04-28 11:28:02 +1000

    transmit the bait in bait detail screen to remove bait screen

--- BaitBit/BaitDetailsViewController.swift
+++ BaitBit/BaitDetailsViewController.swift
@@ -40,14 +40,18 @@ class BaitDetailsViewController: UIViewController {
     func loadBaitImage() {
         
     }
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "RemoveBaitSegue" {
+            let controller = segue.destination as! RemoveBaitTableViewController
+            controller.bait = self.bait
+        }
     }
-    */
+    
 
 }
