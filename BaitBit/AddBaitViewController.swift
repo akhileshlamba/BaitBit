95b749a2799263800c18312830cfe6a4af44a772
Xiaotian LIU <sheldon.liuxiaotian@gmail.com>
2019-04-30 08:44:05 +1000

    delete getLocation button in add bait screen

--- BaitBit/AddBaitViewController.swift
+++ BaitBit/AddBaitViewController.swift
@@ -30,75 +30,22 @@ class AddBaitViewController: UIViewController, CLLocationManagerDelegate {
     let dataSource = ["Date", "Latitude", "Longitude"]
     let imageDateSource = ["date", "latitude", "longitude"]
     var valueDateSource = ["", "", ""]
-    var dateCell: UITableViewCell?
-    var latCell: UITableViewCell?
-    var longCell: UITableViewCell?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        showDatePicker()
-//        location.isEnabled = false
 
         addBaitTableView.delegate = self
         addBaitTableView.dataSource = self
 
         formatter.dateFormat = "MMM dd, yyyy"
         self.valueDateSource[0] = formatter.string(from: Date())
+        
+        self.getLocation()
 
-//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
-
-        self.dateCell = self.addBaitTableView.cellForRow(at: IndexPath(row: 0, section: 0))
-        self.latCell = self.addBaitTableView.cellForRow(at: IndexPath(row: 1, section: 0))
-        self.longCell = self.addBaitTableView.cellForRow(at: IndexPath(row: 2, section: 0))
 
     }
 
-//    @objc func done() {
-//        let controller = self.navigationController?.viewControllers[2]
-//
-//        if controller is AddProgramViewController {
-//            let programTableViewController = self.navigationController?.viewControllers[1]
-//            self.navigationController?.popToViewController(programTableViewController!, animated: true)
-//        } else {
-//            self.navigationController?.popViewController(animated: true)
-//        }
-//    }
-
-//    func showDatePicker(){
-//
-//        datePicker.datePickerMode = .date
-//        datePicker.minimumDate = Date()
-//
-//        bait_laid_date.isEnabled = false
-//
-//        let toolbar = UIToolbar();
-//        toolbar.sizeToFit()
-//        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(donedatePicker));
-//        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
-//        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelDatePicker));
-//
-//        toolbar.setItems([doneButton,spaceButton,cancelButton], animated: false)
-//
-//        bait_laid_date.inputAccessoryView = toolbar
-//        bait_laid_date.inputView = datePicker
-//
-//    }
-
-//    @objc func donedatePicker(){
-//        if bait_laid_date.isFirstResponder {
-//            formatter.dateStyle = .medium
-//            formatter.timeStyle = .none
-//            bait_laid_date.text = formatter.string(from: datePicker.date)
-//            self.view.endEditing(true)
-//        }
-//    }
-
-//    @objc func cancelDatePicker(){
-//        self.view.endEditing(true)
-//    }
-
-
-    @IBAction func get_Location(_ sender: Any) {
+    func getLocation() {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         locationManager.distanceFilter = 10
@@ -177,8 +124,6 @@ class AddBaitViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let loc = locations.last!
         self.currentLocation = loc.coordinate
-//        self.latCell?.detailTextLabel?.text = String(currentLocation.latitude)
-//        self.longCell?.detailTextLabel?.text = String(currentLocation.longitude)
         self.valueDateSource[1] = String(currentLocation.latitude)
         self.valueDateSource[2] = String(currentLocation.longitude)
         self.addBaitTableView.reloadData()
@@ -339,6 +284,10 @@ extension AddBaitViewController: UITableViewDelegate, UITableViewDataSource {
         cell.imageView?.image = UIImage(named: self.valueDateSource[indexPath.row])
 
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
