1d8060cd1663fef58a7996be18a7ad4b5c8792b3
Xiaotian LIU <sheldon.liuxiaotian@gmail.com>
2019-05-23 14:00:49 +1000

    fix long/lat and photo taking in bait details screen

--- BaitBit/BaitDetailsViewController.swift
+++ BaitBit/BaitDetailsViewController.swift
@@ -23,10 +23,28 @@ class BaitDetailsViewController: UIViewController {
     
     @IBOutlet weak var removeButton: UIButton!
     
+    @IBOutlet weak var locationTableView: UITableView!
+    
+    @IBOutlet weak var takePhotoButton: UIButton!
+    
+    
+    var actionSheet: UIAlertController?
+    
+    let dataSource = ["Latitude", "Longitude"]
+    let imageDataSource = ["latitude", "longitude"]
+    var valueDataSource = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setFields()
         self.loadBaitImage()
+        
+        self.locationTableView.delegate = self
+        self.locationTableView.dataSource = self
+        self.locationTableView.tableFooterView = UIView(frame: .zero)
+        
+        self.valueDataSource.append("\(self.bait.latitude)")
+        self.valueDataSource.append("\(self.bait.longitude)")
 
         // Do any additional setup after loading the view.
     }
@@ -56,8 +74,18 @@ class BaitDetailsViewController: UIViewController {
     }
     
     func loadBaitImage() {
-        FirestoreDAO.fetchImage(for: self.bait) { (image) in
-            self.baitImageView.image = image
+        if self.bait.isRemoved {
+            self.takePhotoButton.isHidden = true
+        } else if self.bait.photoPath == nil {
+            self.takePhotoButton.setTitle("Take a photo", for: .normal)
+        } else {
+            self.takePhotoButton.setTitle("", for: .normal)
+            self.loading.startAnimating()
+            FirestoreDAO.fetchImage(for: self.bait) { (image) in
+                self.loading.stopAnimating()
+                self.baitImageView.image = image
+            }
+
         }
     }
     
@@ -74,4 +102,103 @@ class BaitDetailsViewController: UIViewController {
     }
     
 
+}
+
+extension BaitDetailsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AddBaitCell", for: indexPath)
+        cell.textLabel?.text = self.dataSource[indexPath.row]
+        cell.detailTextLabel?.text = self.valueDataSource[indexPath.row]
+        cell.imageView?.image = UIImage(named: self.imageDataSource[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+}
+
+extension BaitDetailsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBAction func displayPhotoOptions(_ sender: Any) {
+    // create an actionSheet
+    self.actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+    
+    // add a Take Photo option
+    self.actionSheet!.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { (_) in
+    self.takePhoto()
+    }))
+    
+    // add a Choose from Album option
+    self.actionSheet!.addAction(UIAlertAction(title: "Choose from Album", style: .default, handler: { (_) in
+    self.chooseFromAlum()
+    }))
+    
+    // add a Cancel option
+    self.actionSheet!.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+    
+    // display the actionSheet
+    self.present(self.actionSheet!, animated: true, completion: nil)
+    }
+    
+    func takePhoto() {
+        let controller = UIImagePickerController()
+        guard UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) else {
+            Util.displayErrorMessage(view: self, "Camera unvailable", "Error")
+            return
+        }
+        
+        controller.sourceType = UIImagePickerController.SourceType.camera
+        controller.allowsEditing = true
+        controller.delegate = self
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    func chooseFromAlum() {
+        let controller = UIImagePickerController()
+        
+        controller.sourceType = UIImagePickerController.SourceType.photoLibrary
+        controller.allowsEditing = true
+        controller.delegate = self
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let actionSheet = self.actionSheet {
+            actionSheet.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - ImagePickerController Delegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("did finish picking photo.")
+        if let pickedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            self.loading.startAnimating()
+            FirestoreDAO.updateImageAndData(for: self.bait, image: pickedImage) { (success) in
+                self.loading.stopAnimating()
+                if success {
+                    print("did get into the if statement")
+                    self.baitImageView.image = pickedImage
+                } else {
+                    Util.displayErrorMessage(view: self, "Fail to update photo", "Error")
+                }
+            }
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        Util.displayErrorMessage(view: self, "There was an error in getting the photo", "Error")
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
